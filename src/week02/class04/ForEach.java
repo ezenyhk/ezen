be1907b865f81a0abaf862a379d15d42e3bd8623
yhk <ezenyhk@gmail.com>
2020-09-17 17:56:07 +0900

    2020/09/17 수업 마무리

--- src/week02/class04/ForEach.java
+++ src/week02/class04/ForEach.java
@@ -4,12 +4,13 @@ public class ForEach {
 	public static void main(String[] args) {
 		int[] a = {1,2,3};
 		int sum = 0;
-		
-		for (int i : a) {
+		for (int i : a) { // 인덱스용 변수 없이 a의 값 변경 불가
 			i = 1;
+			sum += i;
 		}
 		for (int i = 0; i < a.length; i++) {
 			System.out.println(a[i]);
 		}
+		System.out.println("sum = " + sum);
 	}
 }
